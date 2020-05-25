7ef4dcfd7243d60739ef058b68aa68bd3a1d77cd
Olivier Brunet <45876462+obrunet@users.noreply.github.com>
2020-05-25 21:44:24 +0200

    Create 02.woksheet.scala

---
+++ Scala Essential Training/02.woksheet.scala
@@ -0,0 +1,16 @@
+object worksheetOne {
+  println("Welcome to this Scala worksheet!\n")
+  // it provides the types & values of each variables in a REPL way
+  var x = 5
+  val st = "Scala rocks!"
+  var odds = List(1, 3, 5, 7, 9)
+  2 to 10
+  var y = true
+  val pi = 3.14159
+  
+  def add(a: Int, b: Int) = a + b
+  
+  println(add(4, 9))
+}
+
+worksheetOne
