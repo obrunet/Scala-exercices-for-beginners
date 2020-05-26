711af70ee905e165a3349e66e4281bd2fbe3c159
Olivier Brunet <45876462+obrunet@users.noreply.github.com>
2020-05-26 21:11:58 +0200

    update

---
+++ Scala Essential Training/08.forLoop.scala
@@ -0,0 +1,8 @@
+object loopsPractice {
+  for(x <- 10 to 0 by -1) {
+    println(x)
+  }
+  println("blastOff")
+}
+
+loopsPractice
---
+++ Scala Essential Training/10.forLoopWithList.scala
@@ -0,0 +1,13 @@
+object loopsPractice {
+  var word = "Sunday"
+  for(x <- 0 until word.length) { println(word(x))}
+}
+
+loopsPractice
+/*
+S
+u
+n
+d
+a
+y */
